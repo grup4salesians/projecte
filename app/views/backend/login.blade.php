e8ef4d7b14ba7e38768f93fba28f9c7b77eaef6e
Josep Sarabia <josep.sarabia@gmail.com>
2015-01-20 19:49:35 +0100

    Login Backend V1.0 (Sin BG ni link CSS)

--- app/views/backend/login.blade.php
+++ app/views/backend/login.blade.php
@@ -1,62 +1,85 @@
 <html>
     <head>
         <meta charset="utf-8">
-        {{ HTML::style('css.css'); }}
+        <link href="../css.css" rel="stylesheet" type="text/css"/>
         <title>Login</title>
     </head>
-    <body bgcolor="#E6E6FA">
+<!--    <body bgcolor="#E6E6FA">-->
         <style>
             #backend{
-    width: 320px;
-    height: 270px;
-    padding: 30px;
-    border-radius: 10px;
-    background-color:rgba(192, 185, 185, 0.1);
-    margin:auto;
-    margin-top:50px;
-}
-#formulario{
-    height: 250px;
-    padding: 10px;
-    border-radius:10px;
-    background-color:rgba(192, 185, 185, 0.9);
-    margin: auto;
-}
-.login{
-    font-size: 30px;
-    margin-top: 20px;
-}
-.textbox{
-    width:100%;
-    height:30px;
-}
+                width: 50%;
+                max-width: 500px;
+                min-width: 320px;
+                height: 250px;
+                padding: 30px;
+                border-radius: 10px;
+                border: 2px solid rgba(192, 185, 185, 0.9);
+                background-color:rgba(192, 185, 185, 0.1);
+                    background-image: url('/images/backgroundform.jpg');
+                margin:auto;
+                margin-top:50px;
+                box-shadow: 10px 10px 5px #888888;
+            }
+            #formulario{
+                height: 250px;
+                padding: 10px;
+                border-radius:10px;
+                text-align:center;
+                margin: auto;
+                background-size:100%;
+            }
+            .login{ 
+                font-size: 30px;
+                margin-top: 20px;   
+            }
+            .textbox{
+                width:100%;
+                height:30px;
+                margin-top:10px;
+            }
+            .submit{
+                width:100%;
+                height:50px;
+                margin-top:20px;
+                font-size: 20px;
+                border: 1px solid rgba(192, 185, 185, 0.9);
+                box-shadow: 5px 5px 5px #888888;          
+            }
+            .submit:hover{
+                 background: 1px rgba(192, 185, 185, 0.5);
+            }
+            .submit:active{
+                box-shadow: 2px 2px 2px #888888;
+                position:relative;
+                top:3px;
+            }
         </style>
         <h1 style="text-align:center;">Backend DawSharing</h1>
         <div id="backend">
-            
+
             <div id="formulario">
-                
-            
-               {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
+               
+
+                {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
                 @if(Session::has('mensaje_error'))
                 {{ Session::get('mensaje_error') }}
                 @endif
                 {{ Form::open(array('url' => '/login')) }}
-                <span class="login">{{ Form::label('usuario', 'Nom') }}</span> 
+                {{ Form::label('usuario', 'Nom',array('class'=>'login')) }}
                 <br>
-                <span class="textbox"> {{ Form::text('username', Input::old('username')); }}</span>
+                {{ Form::text('username',Input::old('username'),array('class'=>'textbox')); }}
                 <br>
-                <span class="login"> {{ Form::label('contrasenya', 'Contrasenya') }}</span> 
+                {{ Form::label('contrasenya', 'Contrasenya',array('class'=>'login'));}}
                 <br>
-              <span class="textbox">  {{ Form::password('password'); }}</span>
+                {{ Form::password('password',array('class'=>'textbox')); }}
                 <br>
-                {{ Form::label('lblRememberme', 'Recordar contraseña') }}
+                {{ Form::label('lblRememberme', 'Recordar contraseña'); }}
                 {{ Form::checkbox('rememberme', true) }}
                 <br>
-                {{ Form::submit('Enviar') }}
+                {{ Form::submit('Entrar',array('class'=>'submit')) }}
                 {{ Form::close() }}
-        
+
+            </div>
         </div>
-         </div>
     </body>
 </html>
