a3455fd567afbf3a42fc3b647e31a5d68bebb996
Josep Sarabia <josep.sarabia@gmail.com>
2015-01-20 20:49:37 +0100

    Estructura de carpetes, images i css a public, login backend v2.0

--- app/views/backend/login.blade.php
+++ app/views/backend/login.blade.php
@@ -1,64 +1,16 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link href="../css.css" rel="stylesheet" type="text/css"/>
+        {{ HTML::style('css/css.css'); }}
         <title>Login</title>
     </head>
-<!--    <body bgcolor="#E6E6FA">-->
-        <style>
-            #backend{
-                width: 50%;
-                max-width: 500px;
-                min-width: 320px;
-                height: 250px;
-                padding: 30px;
-                border-radius: 10px;
-                border: 2px solid rgba(192, 185, 185, 0.9);
-                background-color:rgba(192, 185, 185, 0.1);
-                    background-image: url('/images/backgroundform.jpg');
-                margin:auto;
-                margin-top:50px;
-                box-shadow: 10px 10px 5px #888888;
-            }
-            #formulario{
-                height: 250px;
-                padding: 10px;
-                border-radius:10px;
-                text-align:center;
-                margin: auto;
-                background-size:100%;
-            }
-            .login{ 
-                font-size: 30px;
-                margin-top: 20px;   
-            }
-            .textbox{
-                width:100%;
-                height:30px;
-                margin-top:10px;
-            }
-            .submit{
-                width:100%;
-                height:50px;
-                margin-top:20px;
-                font-size: 20px;
-                border: 1px solid rgba(192, 185, 185, 0.9);
-                box-shadow: 5px 5px 5px #888888;          
-            }
-            .submit:hover{
-                 background: 1px rgba(192, 185, 185, 0.5);
-            }
-            .submit:active{
-                box-shadow: 2px 2px 2px #888888;
-                position:relative;
-                top:3px;
-            }
-        </style>
-        <h1 style="text-align:center;">Backend DawSharing</h1>
+    <body>
+        
+        <h1 style="text-align:center;">Backend DawSharing Grup 4</h1>
         <div id="backend">
 
             <div id="formulario">
-               
+
 
                 {{-- Preguntamos si hay algún mensaje de error y si hay lo mostramos  --}}
                 @if(Session::has('mensaje_error'))
@@ -81,5 +33,6 @@
 
             </div>
         </div>
+        <div id="fondo"></div>
     </body>
 </html>
